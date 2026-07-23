a80fc3b902a3221950e9a3a8c1bba83f965c5eed
Duvan Alirio Parra Pinto <duvan2002pinto@gmail.com>
2026-07-22 19:30:02 -0500

    7:30pm

--- innerJoin.sql
+++ innerJoin.sql
@@ -13,13 +13,20 @@ select * from informe;
 select * from comprobante;
 select * from historial;
 
-ALTER TABLE usuarios AUTO_INCREMENT = 2;
+ALTER TABLE orden_servicio AUTO_INCREMENT = 1;
+ALTER TABLE detalles_orden_servicio AUTO_INCREMENT = 1;
+ALTER TABLE informe AUTO_INCREMENT = 1;
+ALTER TABLE usuarios AUTO_INCREMENT = 3;
+ALTER TABLE historial AUTO_INCREMENT = 1;
 
 UPDATE servicios
 SET ID_SERVICIOS = '5'
 WHERE ID_SERVICIOS = 2;
 
-DELETE FROM usuarios WHERE id_usuario = 2;
+DELETE FROM orden_servicio WHERE id_orden = 1;
+DELETE FROM detalles_orden_servicio WHERE id_detalle = 1;
+DELETE FROM usuarios WHERE id_usuario = 8;
+
 
 SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE TABLE usuarios;
